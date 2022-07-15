33c4e374da4943ae98d4a440f91ef04b363447a2
Anuj <80551063+codeanuj@users.noreply.github.com>
2022-07-15 11:26:08 +0200

    Update Solution.java

--- Math/171 Excel Sheet Column Number/Solution.java
+++ Math/171 Excel Sheet Column Number/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         double result=0;
         int pow=0;
         for(int i=columnTitle.length()-1; i>=0; i--){
-            
+            /*adding 1 as A corresponds to 1 so if A comes then A-A==0 but it should be one so +1 */
             double a = columnTitle.charAt(i)-'A'+1;
             double power=Math.pow(26, pow);
             a=a*power;
