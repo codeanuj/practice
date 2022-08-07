8c6cbe7b638b06cd1df0c295b75180faa989f5b7
Anuj <80551063+codeanuj@users.noreply.github.com>
2022-08-07 23:06:55 +0530

    Create Solution.java

---
+++ Sorting/Unique Elements/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int solve(int[] A) {
+        
+     int n = A.length;
+        // sort the array
+        Arrays.sort(A);
+        // initialize variables
+        int steps = 0, i = 1, j = 0;
+        // loop unitil you reach the end
+        while (j < n) {
+            // make current element unique
+            if (i >= A[j]) {
+                steps += (i - A[j++]);
+                i += 1;
+            } else {
+                i = A[j] + 1;
+                j += 1;
+            }
+        }
+        // return the answer
+        return steps; 
+}
